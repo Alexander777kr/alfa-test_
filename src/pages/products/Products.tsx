8754569264df277932a8c7321996d7983e83c6b2
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-09-28 21:48:39 +0300

    add products page

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,3 +1,15 @@
+import { Container, Grid } from '@chakra-ui/react';
+import Card from '../../components/card/Card';
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <Container maxW="1200px" px={4} py={16}>
+      <Grid templateColumns="repeat(3, minmax(250px, 1fr))" gap={6}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
+    </Container>
+  );
 }
